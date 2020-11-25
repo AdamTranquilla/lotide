b319287d13100c3e119bded8a855a38525450377
Adam Tranquilla <adam.tranquilla@gmail.com>
2020-11-25 23:07:55 +0000

    countt letters complete

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const countLetters = (str) => {
+  const results = {};
+  for (const letter of str) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results
+};
