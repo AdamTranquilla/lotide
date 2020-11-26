a8979838750010f17976c9c521cfbabb1c800a4a
Adam Tranquilla <adam.tranquilla@gmail.com>
2020-11-26 00:07:51 +0000

    is this the best movie in the genre

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKeyByValue = (bestTV, candidate) => {
+  for (const genre in bestTV) {
+    if ( bestTV[genre] === candidate) {
+      return genre
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
