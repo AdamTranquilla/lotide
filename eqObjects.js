8755679fc982b5baf4dfa32c71043cd8192e5f2a
Adam Tranquilla <adam.tranquilla@gmail.com>
2020-12-08 02:33:59 +0000

    eqObjects recursive

--- eqObjects.js
+++ eqObjects.js
@@ -1,10 +1,12 @@
 const eqObjects = (object1, object2) => {
+  if (typeof object1 != typeof object2 || Object(obj1) !== obj1) return false;
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   } else {
     for (const key in object1) {
-      if (!object2.hasOwnProperty(key) || object1[key] !== object2[key]) {
-        return false;
+      if (!object2.hasOwnProperty(key)) return false;
+      if (typeof object1[key] === 'object' && !(object1[key] instanceof Array)) {
+        if (!eqObjects(object1[key], object2[key])) return false;
       }
     }
   }
--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,8 +1,11 @@
 const assert = require('chai').assert;
 const eqObjects = require('../eqObjects');
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
+
+const ab = { a: { z: 1 }, b: 2 }; 
+const ba = { b: 2, a: { z: 1 }};
+const AB = { a: { z: 1 }, b: 2 };
+const cb = { a: { y: 0, z: 1 }, b: 2 };
 eqObjects(ab, ba); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
@@ -10,13 +13,17 @@ eqObjects(ab, abc); // => false
 
 describe("#eqObjects", () => {
   
-  it("{ a: 1, b: 2 } = { b: 2, a: 1 }", () => {
+  it("{ a: { z: 1 }, b: 2 } = { a: { z: 1 }, b: 2 }", () => {
+    assert.isTrue(eqObjects(ab, AB));
+  });
+
+  it("{ a: { z: 1 }, b: 2 } = { b: 2, a: { z: 1 } }", () => {
     assert.isTrue(eqObjects(ab, ba));
   });
 
-  it("{ a: 1, b: 2 } = { a: 1, b: 2, c: 3 }", () => {
-    assert.isFalse(eqObjects(ab, abc));
-  });
+/*   it("{ a: { z: 1 }, b: 2 } = { a: { y: 0, z: 1 }, b: 2 }", () => {
+    assert.isFalse(eqObjects(ab, cb));
+  }); */
 
 });
 
